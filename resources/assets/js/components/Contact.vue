a76c57aebc25f07de7e250a263030e8633031d08
Mani Maran <manimaran205.mm@gmail.com>
2018-09-21 10:56:35 +0530

    Initial Commit

---
+++ resources/assets/js/components/Contact.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Hello from about page</h1>
+</template>
+
+<script>
+    export default {
+        name: "Contact"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/assets/js/router.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+Vue.use(Router);
+
+export default new Router({
+    mode: 'history',
+    routes: [
+        { path: '/', component: Home, name: 'home' },
+        { path: '/about', component: About, name: 'about' },
+        { path: '/contact', component: Contact, name: 'contact' }
+    ]
+});
